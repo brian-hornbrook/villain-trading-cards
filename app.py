d539acb551f6e3537798910fb20c3d3981ea4ff3
brian-hornbrook <brian_hornbrook@outlook.com>
2022-07-29 14:31:29 -0500

    created SQL database

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("app")
 
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///villain.db"
+
+db = SQLAlchemy(app)
 
 @app.route("/")
-def hello_world():
-    return render_template("villain.html")
-
+def villains_cards():
+  return render_template("villain.html")
 
 app.run(host='0.0.0.0', port=8080)
