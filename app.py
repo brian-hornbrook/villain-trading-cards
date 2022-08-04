1b9a360c6b4f6c827eb9d7eb88402bea9947e416
brian-hornbrook <brian_hornbrook@outlook.com>
2022-08-04 14:55:07 -0500

    added delete functionality

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Villain(db.Model):
 	url = db.Column(db.String(250), nullable=False)
 	date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-	def __repr__(self, ):
+	def __repr__(self):
 		return f"<Villain {self.name}>"
 
 db.create_all()
@@ -59,5 +59,20 @@ def add_user():
 		db.session.add(new_villain)
 		db.session.commit()
 		return render_template("villain.html", villains=Villain.query.all())
+
+@app.route("/delete", methods=["GET"])
+def delete_villain():
+	return render_template("deletevillain.html", errors=[])
+
+@app.route("/deleteVillain", methods=["POST"])
+def delete_user():
+	name = request.form.get("name")
+	villain = Villain.query.filter_by(name=name).first()
+	if villain:
+		db.session.delete(villain)
+		db.session.commit()
+		return render_template("villain.html", villains=Villain.query.all())
+	else:
+		return render_template("deletevillain.html", errors=["Oops! That villain doesn't exist!"])
 
 app.run(host='0.0.0.0', port=8080)
