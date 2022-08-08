53392c1e094aac3fd5ce3ed38095c79ebab8edc0
brian-hornbrook <brian_hornbrook@outlook.com>
2022-08-08 15:22:01 -0500

    changed the endpoints to be restful

--- app.py
+++ app.py
@@ -1,78 +1,89 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask("app")
-
-app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///villain.db"
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///villain.db"
 db = SQLAlchemy(app)
 
 class Villain(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name =db.Column(db.String(80), unique=True, nullable=False)
-	description = db.Column(db.String(250), nullable=False)
-	interests = db.Column(db.String(250), nullable=False)
-	url = db.Column(db.String(250), nullable=False)
-	date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    interests = db.Column(db.String(250), nullable=False)
+    url = db.Column(db.String(250), nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-	def __repr__(self):
-		return f"<Villain {self.name}>"
+    def __repr__(self):
+        return "<Villain "+ self.name + ">"
 
 db.create_all()
 db.session.commit()
-	
 
+#### Serving Static Files
 @app.route("/")
-def villains_cards():
-  return render_template("villain.html", villains=Villain.query.all())
+def villain_cards():
+  return app.send_static_file("villain.html")
 
-@app.route("/add", methods=["GET"])
+# Villains
+@app.route("/api/villains", methods=["GET"])
+def get_villains():
+	villains = Villain.query.all()
+	data = []
+	return
+
+@app.route("/add")
+def add():
+  return app.send_static_file("addvillain.html")
+
+@app.route("/delete")
+def delete():
+  return app.send_static_file("deletevillain.html")
+####
+
+#ADD CODE: add /api/villains route here
+
+@app.route("/api/villains/add", methods=["POST"])
 def add_villain():
-	errors = []
-	return render_template("addvillain.html", errors=errors)
+  errors = []
+  name = request.form.get("name")
 
-@app.route("/addVillain", methods=["POST"])
-def add_user():
-	errors = []
-	name = request.form.get("name")
-	if not name:
-		errors.append("Oops! Looks like you forgot a name!")
-	description = request.form.get("description")
-	if not description:
-		errors.append("Oops! Looks like you forgot a description!")
-	interests = request.form.get("interests")
-	if not interests:
-		errors.append("Oops! Looks like you forgot some interests!")
-	url = request.form.get("url")
-	if not url:
-		errors.append("Oops! Looks like you forgot an image!")
+  if not name:
+    errors.append("Oops! Looks like you forgot a name!")
 
-	villain = Villain.query.filter_by(name=name).first()
-	if villain:
-		errors.append("Oops! A villain with that name already exists!")
+  description = request.form.get("description")
+  if not description:
+    errors.append("Oops! Looks like you forgot a description!")
+  
+  interests = request.form.get("interests")
+  if not interests:
+    errors.append("Oops! Looks like you forgot some interests!")
+  
+  url = request.form.get("url")
+  if not url:
+    errors.append("Oops! Looks like you forgot an image!")
+  
+  villain = Villain.query.filter_by(name=name).first()
+  if villain:
+    errors.append("Oops! A villain with that name already exists!")
+  
+  if errors:
+    return jsonify({"errors": errors})
+  else:
+    new_villain = Villain(name=name,description=description, interests=interests, url=url)
+    db.session.add(new_villain)
+    db.session.commit()
+    return
 
-	if errors:
-		return render_template("addvillain.html", errors=errors)
-	else:
-		new_villain = Villain(name=name, description=description, interests=interests, url=url)
-		db.session.add(new_villain)
-		db.session.commit()
-		return render_template("villain.html", villains=Villain.query.all())
-
-@app.route("/delete", methods=["GET"])
+@app.route("/api/villains/delete", methods=["POST"])
 def delete_villain():
-	return render_template("deletevillain.html", errors=[])
-
-@app.route("/deleteVillain", methods=["POST"])
-def delete_user():
-	name = request.form.get("name")
-	villain = Villain.query.filter_by(name=name).first()
-	if villain:
-		db.session.delete(villain)
-		db.session.commit()
-		return render_template("villain.html", villains=Villain.query.all())
-	else:
-		return render_template("deletevillain.html", errors=["Oops! That villain doesn't exist!"])
+  name = request.form.get("name", "")
+  villain = Villain.query.filter_by(name=name).first()
+  if villain:
+    db.session.delete(villain)
+    db.session.commit()
+    return
+  else:
+    return jsonify({"errors": ["Oops! A villain with that name doesn't exist!"]})
 
 app.run(host='0.0.0.0', port=8080)
